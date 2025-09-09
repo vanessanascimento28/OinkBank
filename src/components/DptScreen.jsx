84de6c0d26020e6e32a89e5cbcbf964dc9b70aac
Vanessa Nascimento <vanessa.ldr.n@gmail.com>
2025-09-09 16:44:06 -0300

    "deposit screen header and main"

--- src/components/DptScreen.jsx
+++ src/components/DptScreen.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import "../blocks/DptScreen.css";
+import blueWallet from "../images/blueWallet.svg";
+import blueArrow from "../images/blueArrow.svg";
+import petPig from "../images/petPig.svg";
 
 function DptScreen() {
+  const balance = 180.0;
+  const balanceBRL = balance.toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
   return (
     <div className="dpt-screen">
-      <h1>💰 Depósito</h1>
-      <p>Aqui vai o conteúdo da tela de depósito.</p>
+      <header className="dpt__header">
+        <img className="icon-wallet" src={blueWallet} alt="Carteira" />
+
+        <span className="dpt__balance" aria-label="Saldo">
+          R$ {balanceBRL}
+        </span>
+
+        <img src={blueArrow} alt="seta azul" className="icon-arrow" />
+        <p className="dpt__avbalance">
+          Seu saldo <br /> disponível
+        </p>
+      </header>
+      <main className="dpt__content">
+        <div className="dpt__pet-wrapper">
+          <img src={petPig} alt="Porquinho mascote" className="dpt__pet" />
+          <div className="dpt__pet-shadow" aria-hidden="true"></div>
+        </div>
+        <h1 className="dpt__title">
+          Quanto você quer <br /> guardar no porquinho?
+        </h1>
+        <input type="text" className="dpt__input" placeholder="R$ 000,00" />
+      </main>
+      <footer className="dpt__footer">
+        <p className="dpt__footer-title">🏦Oink Bank</p>
+      </footer>
     </div>
   );
 }

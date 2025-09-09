c80b24adcac7d6cc6e083ecca9ac864c5e6db2b9
Vanessa Nascimento <vanessa.ldr.n@gmail.com>
2025-09-09 16:58:12 -0300

    "deposit screen buttons added"

--- src/components/DptScreen.jsx
+++ src/components/DptScreen.jsx
@@ -3,6 +3,8 @@ import "../blocks/DptScreen.css";
 import blueWallet from "../images/blueWallet.svg";
 import blueArrow from "../images/blueArrow.svg";
 import petPig from "../images/petPig.svg";
+import closeRed from "../images/closeRed.svg";
+import confirmGreen from "../images/confirmGreen.svg";
 
 function DptScreen() {
   const balance = 180.0;
@@ -34,6 +36,25 @@ function DptScreen() {
           Quanto você quer <br /> guardar no porquinho?
         </h1>
         <input type="text" className="dpt__input" placeholder="R$ 000,00" />
+        <div className="dpt__actions">
+          <button
+            className="dpt__btn"
+            type="button"
+            aria-label="Fechar"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img src={closeRed} alt="Cancelar" className="dpt__btn-icon" />
+          </button>
+
+          <button
+            className="dpt__btn"
+            type="button"
+            aria-label="Confirmar"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img src={confirmGreen} alt="Confirmar" className="dpt__btn-icon" />
+          </button>
+        </div>
       </main>
       <footer className="dpt__footer">
         <p className="dpt__footer-title">🏦Oink Bank</p>

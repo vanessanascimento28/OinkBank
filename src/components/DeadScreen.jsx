d0a3a6e41c86856b8e91f19044ab5f56f42d6d8e
Vanessa Nascimento <vanessa.ldr.n@gmail.com>
2025-09-11 17:52:42 -0300

    "dead screen"

--- src/components/DeadScreen.jsx
+++ src/components/DeadScreen.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../blocks/DeadScreen.css";
 
+import deadPig from "../images/deadPig.svg";
+import okButton from "../images/okButton.svg";
+
 function DeadScreen() {
+  const navigate = useNavigate();
+
   return (
-    <div className="dead-screen" />
+    <div className="dead-screen">
+      <main className="dead__content">
+        <div className="dead__pig-area">
+          <img src={deadPig} alt="Porquinho quebrado" className="dead__pig" />
+          <div className="dead__pig-shadow" aria-hidden="true"></div>
+        </div>
+
+        <h1 className="dead__title">Porquinho Quebrado!</h1>
+        <p className="dead__subtitle">Seu dinheiro está na conta.</p>
+
+        <button
+          className="dead__ok-btn"
+          type="button"
+          onClick={() => navigate("/")}
+          aria-label="Voltar para a Home"
+        >
+          <img src={okButton} alt="OK" className="dead__ok-icon" />
+        </button>
+      </main>
+    </div>
   );
 }
 
 export default DeadScreen;
+
